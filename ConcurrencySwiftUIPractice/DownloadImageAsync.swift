b3f3448c3b813f6082217c7473c714615163a474
Max Valek <valek.maxwell@gmail.com>
2023-05-16 12:18:56 -0500

    initial commit

---
+++ ConcurrencySwiftUIPractice/DownloadImageAsync.swift
@@ -0,0 +1,22 @@
+//
+//  DownloadImageAsync.swift
+//  ConcurrencySwiftUIPractice
+//
+//  Created by Max Valek on 5/16/23.
+//
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
