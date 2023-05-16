f45cfe731351758f5489412ca260ba9a4afb0a9b
Max Valek <valek.maxwell@gmail.com>
2023-05-16 12:16:35 -0500

    Initial Commit

---
+++ ConcurrencySwiftUIPractice/ConcurrencySwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencySwiftUIPracticeApp.swift
+//  ConcurrencySwiftUIPractice
+//
+//  Created by Max Valek on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencySwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
